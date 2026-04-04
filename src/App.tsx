3ab3e8b32dff1ad417bf97c1847b7fc1f0ba01fb
hateNaldoz <aguilarnald@gmail.com>
2026-04-03 20:14:14 -0700

    revamp

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Header from "./components/header/header";
 import UploadMP3 from "./components/upload/upload_mp3";
+
 function App() {
     return (
-        <>
+        <div className="bg-[#0F172A] min-h-screen text-slate-50 font-[Poppins]">
             <Header />
-            <main className="relative h-screen w-full">
+            <main className="relative min-h-screen w-full">
                 <div
                     id="main-wrapper"
                     className="h-full w-full"
@@ -12,7 +13,7 @@ function App() {
                     <UploadMP3 />
                 </div>
             </main>
-        </>
+        </div>
     );
 }
 
